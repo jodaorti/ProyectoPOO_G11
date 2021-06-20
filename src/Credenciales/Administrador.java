9f5820a249fa9cf81dd91cbf4d01b1b07cbc39a2
jodaorti <jodaorti@espol.edu.ec>
2021-06-20 04:05:21 -0500

    Agregando la clase Administrador

---
+++ src/Credenciales/Administrador.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Credenciales;
+
+/**
+ * 
+ * @author Joffre Ortiz
+ */
+public class Administrador extends Usuario {
+    /**
+     * Se registra la propiedad del cliente
+     */
+    public void registrarPropiedad(){
+        
+    }
+    /**
+     * Se registra el agente con el cual se hará la venta
+     * @param nombre
+     * @param cedula
+     * @param correo 
+     */
+    public void registrarAgente(String nombre,String cedula,String correo){
+        
+    }
+    /**
+     * Se genera un reporte actualizado con las ventas y su debido contacto con el agente
+     * @param finicio
+     * @param Ffinal 
+     */
+    public void reporteContactoyVentas(String finicio,String Ffinal){
+        
+    }
+
+}
