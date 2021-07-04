94b480241b414593fba73631b6eb85a4ffd69022
Marina Perez <maralper@espol.edu.ec>
2021-07-04 18:14:12 -0500

    Metodo que carga la información del sistema por defecto

--- src/utils/ClassUtils.java
+++ src/utils/ClassUtils.java
@@ -4,7 +4,12 @@ import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import modelo.AgenteVentas;
+import modelo.Casa;
 import modelo.Cliente;
+import modelo.Sistema;
+import modelo.Terreno;
+import modelo.Ubicacion;
+import tipos.Enum.tipoTerreno;
 
 public class ClassUtils {
     
@@ -12,9 +17,12 @@ public class ClassUtils {
     * Carga los usuarios previo a ejecucion del sistema:
     * Crear dos agentes de ventas, un cliente y 4 propiedades: dos terrenos y dos casas
     * Dos de estas propiedades deben estar en la misma ciudad y sector. 
+     * @return 
     */
-    public static void cargarInformacionDefecto()
+    public static Sistema cargarInformacionDefecto()
     {
+        Sistema sistema = new Sistema();
+        
         String usuario,contrasenia,codigoAgente,nombre,cedula,email;        
         LocalDate fechaNacimiento;
         
@@ -28,6 +36,7 @@ public class ClassUtils {
         cedula = "1234";
         email  = "marina@gmail.com";
         AgenteVentas ag1 = new AgenteVentas(usuario,contrasenia,codigoAgente,nombre,cedula,email);
+        sistema.setListaAgentes(ag1);
 
         //Agente 2 
         usuario = "ag2";
@@ -37,6 +46,7 @@ public class ClassUtils {
         cedula = "2222";
         email  = "max@gmail.com";
         AgenteVentas ag2 = new AgenteVentas(usuario,contrasenia,codigoAgente,nombre,cedula,email);
+        sistema.setListaAgentes(ag2);
         
         
         // Crear cliente       
@@ -47,8 +57,52 @@ public class ClassUtils {
         email  = "marta@gmail.com";
         fechaNacimiento = StringToDate("2001-04-10");
         Cliente cli1 = new Cliente(usuario, contrasenia, nombre, cedula,email, fechaNacimiento);
+        sistema.setListaClientes(cli1);
         
+        String codigoPropiedad;
+        double precio,metrosAncho,profundidad;
+        int numPisos,numHabitaciones;
+        Ubicacion ubicacion;
         
+        //Crear terrenos
+        codigoPropiedad = "pr1";
+        precio = 10000;
+        metrosAncho = 60;
+        profundidad = 10;        
+        ubicacion = new Ubicacion("Guayas","Guayaquil","cdla. Sta Cecilia","norte");
+        Terreno ter1 = new Terreno(codigoPropiedad,precio,metrosAncho,profundidad,ubicacion,tipoTerreno.VIVIENDA);
+        sistema.setListaPropiedades(ter1);
+        
+        codigoPropiedad = "pr2";
+        precio = 15000;
+        metrosAncho = 80;
+        profundidad = 20;        
+        ubicacion = new Ubicacion("Guayas","Guayaquil","cdla. Alborada","norte");
+        Terreno ter2 = new Terreno(codigoPropiedad,precio,metrosAncho,profundidad,ubicacion,tipoTerreno.EMPRESARIAL);
+        sistema.setListaPropiedades(ter2);
+        
+        //Crear Casas
+        codigoPropiedad = "pr3";
+        precio = 17000;
+        metrosAncho = 70;
+        profundidad = 30;        
+        ubicacion = new Ubicacion("Pichincha","Quito","cdla. Concordia","norte");
+        numPisos = 2;
+        numHabitaciones = 3;
+        Casa casa1 = new Casa(codigoPropiedad,precio,metrosAncho,profundidad,ubicacion,numPisos,numHabitaciones);
+        sistema.setListaPropiedades(casa1);
+        
+        codigoPropiedad = "pr4";
+        precio = 8000;
+        metrosAncho = 30;
+        profundidad = 30;        
+        ubicacion = new Ubicacion("Pichincha","Quito","cdla. Macarena","norte");
+        numPisos = 1;
+        numHabitaciones = 2;
+        Casa casa2 = new Casa(codigoPropiedad,precio,metrosAncho,profundidad,ubicacion,numPisos,numHabitaciones);
+        sistema.setListaPropiedades(casa2);
+        
+        return sistema;
     }        
     
     public static LocalDate StringToDate(String fecha)
